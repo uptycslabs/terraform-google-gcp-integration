c9fbb67571486f4cc74b1f301ceeeb072146499b
Gopal Bonthu <62247609+gbonthu@users.noreply.github.com>
2022-08-05 20:38:15 +0000

    CSM-4547: Configure resources under existing project. Do not add new … (#9)
    * CSM-4547: Configure resources under existing project. Do not add new folder and/or project just for integration
    
    * CSM-4547: Update README and lint
    
    * CSM-4547: comments

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,12 @@
 output "host-project-id" {
-  value = google_project.my_host_project.project_id
+  value = data.google_project.my_host_project.project_id
 }
 
-output "regenerate-cred-config-command" {
-  description = "For creating again same cred config json file"
-  value       = "gcloud iam workload-identity-pools create-cred-config projects/${google_project.my_host_project.number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.create_wip.workload_identity_pool_id}/providers/${google_iam_workload_identity_pool_provider.add_provider.workload_identity_pool_provider_id} --service-account=${google_service_account.sa_for_hostproject.email} --output-file=credentials.json --aws"
+output "integration-name" {
+  value = var.integration_name
 }
 
-output "integration-projects-list" {
-  value = var.integration_projects == "" ? join(",",data.google_projects.my-org-projects.projects[*].project_id) : var.integration_projects
-}
-
-output "integration-projects-list-command" {
-  value = var.integration_projects == "" ? "gcloud projects list --filter 'lifecycleState: ACTIVE AND projectId != ${google_project.my_host_project.project_id}' --format=\"json\" | jq -c" : "gcloud projects list --filter=${join(",",formatlist("'project_id:%s'" ,replace(var.integration_projects, "," , " OR project_id:")))} --format=\"json\" | jq -c"
+output "credential-file-gen-command" {
+  description = "Command to generate the credentials JSON file"
+  value       = "gcloud iam workload-identity-pools create-cred-config projects/${data.google_project.my_host_project.number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.create_wip.workload_identity_pool_id}/providers/${google_iam_workload_identity_pool_provider.add_provider.workload_identity_pool_provider_id} --service-account=${google_service_account.sa_for_hostproject.email} --output-file=credentials.json --aws"
 }
--- variables.tf
+++ variables.tf
@@ -1,38 +1,30 @@
 variable "organization_id" {
-  type        = string
+  type = string
   description = "The GCP parent organization ID that is getting integrated"
 }
 
-variable "integration_projects" {
-  type        = string
-  description = "List of projects to be integrated"
-  default     = ""
+variable "host_project_id" {
+  type = string
+  description = "Project ID to host resources created as part of integration."
 }
 
 variable "integration_name" {
-  type        = string
-  description = "Unique phrase. Used to name resources created by the plan"
-}
-
-variable "host_project_tags" {
-  default     = {}
-  description = "(Optional) host project tags"
-  type        = map(string)
+  type = string
+  description = "Unique tag. Used to name resources created by the plan"
 }
 
 variable "service_account_name" {
-  type        = string
+  type = string
   description = "The GCP service account name."
-  default     = "sa-for-uptycs"
 }
 
 variable "host_aws_account_id" {
-  type        = string
-  description = "The deployer host AWS account ID."
+  type = string
+  description = "AWS account ID of Uptycs - for identity federation."
 }
 
-variable "host_aws_instance_role" {
-  type        = string
-  description = "The attached deployer host AWS role name."
+variable "host_aws_instance_roles" {
+  type = list
+  description = "AWS roles of Uptycs - for identity binding."
 }
 
