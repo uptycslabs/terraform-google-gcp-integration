8cd913fd7cf89324e1a1aad13f3cbf0945da69fe
lakshman-uptycs <llukalapu@uptycs.com>
2022-09-25 19:39:15 +0530

    Code change to exclude the projects in gsuite folder

--- main.tf
+++ main.tf
@@ -6,9 +6,26 @@ data "google_project" "my_host_project" {
   project_id = var.host_project_id
 }
 
+data "google_folders" "my-org-folders" {
+  parent_id = "organizations/${var.organization_id}"
+}
+
+data "google_folders" "app_script_folders" {
+  count = length(local.system_gsuite_folders) == 0 ? 0 : 1
+  parent_id = local.system_gsuite_folders[0]
+}
+
 locals {
+  # Get gsuite folder in organization 
+  system_gsuite_folders = [for each in data.google_folders.my-org-folders.folders : each.name if each.display_name == "uptycs"]
+  # Get appscript folder in the gsuite folder
+  system_app_script_folders = length(local.system_gsuite_folders) == 0 ? []: [for each in data.google_folders.app_script_folders[0].folders : each.name if each.display_name == "uptycs-sub-folder1"]
+  
   projects_with_tag = toset([for each in data.google_projects.my-org-projects.projects : each.project_id if contains(keys(each.labels), "uptycs-integration")])
-  all_project_ids = toset([for each in data.google_projects.my-org-projects.projects : each.project_id])
+  
+  # Exclude the projects in the path Organization root > system-gsuite > apps-script
+  all_project_ids = length(local.system_app_script_folders) == 0 ? toset([for each in data.google_projects.my-org-projects.projects : each.project_id]) : toset([for each in data.google_projects.my-org-projects.projects : each.project_id if each.parent.id != (split("/",local.system_app_script_folders[0]))[1]])
+  
   projects_to_integrate = length(local.projects_with_tag) != 0 ? setunion(local.projects_with_tag, [var.host_project_id]) : local.all_project_ids
   /*service_names = [
                      "bigquery", "bigquerymigration", "bigquerystorage", "cloudapis",
--- versions.tf
+++ versions.tf
@@ -20,7 +20,7 @@ terraform {
 
     google = {
       source  = "hashicorp/google"
-      version = "~> 3.61.0"
+      version = ">= 3.61.0"
     }
   }
 
